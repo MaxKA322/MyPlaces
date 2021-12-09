f63326a9d3f8d4a6041e1a810ed44038ad1bd5aa
Konstantin Maksimov <k.a.maximov@gmail.com>
2021-12-09 23:20:35 +0300

    Add model

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
-    let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai",
-                           "Индокитай", "X.O","Балкан Гриль","Sherlock Holmes",
-                            "Speak Easy", "Morris Pub", "Вкусные истории",
-                            "Классик", "Love&Life", "Шок","Бочка"]
+//    let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai",
+//                           "Индокитай", "X.O","Балкан Гриль","Sherlock Holmes",
+//                            "Speak Easy", "Morris Pub", "Вкусные истории",
+//                            "Классик", "Love&Life", "Шок","Бочка"]
+    
+    let places = [
+        Place(name: "Burger Heroes", location: "Moscow", type: "Ресторан", image: "Burger Heroes")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +25,16 @@ class MainViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        cell.nameLabel?.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel?.text    = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text     = places[indexPath.row].type
+        cell.imageOfPlace?.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
         cell.imageOfPlace?.clipsToBounds = true
         
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Константин Максимов on 09.12.2021.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+}
